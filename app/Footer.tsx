8ea6a45d99abe51e5b9d77ab1359a69c1c06b2cf
Jose Quijije <josequijije5@gmail.com>
2023-02-19 14:48:40 -0600

    nextjscommit

---
+++ app/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='bg-slate-900 block px-auto py-12 m-auto text-white font-semibold align-middle w-full'>
+        <p className='text-center'>This is a footer</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ app/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='p-5 text-center text-white font-medium bg-blue-900 uppercase'>this is a header for the website</div>
+  )
+}
+
+export default Header
